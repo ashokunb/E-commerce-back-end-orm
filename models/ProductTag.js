e5d6f1e611df1dc491a6be7369005fee270569a3
Ambrose Shokunbi <shokunbitobi@gmail.com>
2022-05-28 19:11:33 -0500

    defined model elements, need to create association with index.js

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,12 +1,33 @@
 const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection');
+const Product = require('./Product');
 
 class ProductTag extends Model {}
 
 ProductTag.init(
   {
     // define columns
+    Id:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    tag_id: DataTypes.INTEGER,
+    allowNull: false,
+    validate: {isNumeric: true},
+    reference: {
+      model: "tag",
+      key: "id"
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "product",
+        key: "id"
+      }
+    }
   },
   {
     sequelize,
